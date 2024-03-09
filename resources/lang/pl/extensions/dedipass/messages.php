0747582bc9be3a65263415503ab3285165d1220a
Elit Gaimix <elitgaimix@gmail.com>
2024-03-09 21:26:03 +0100

    first push

--- resources/lang/pl/extensions/dedipass/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'setup' => 'Na pulpicie Dedipass musisz wybrać walutę wirtualną, ustaw adres URL instalacji na :url, a następnie wybierz oddzwanianie IPN i ustaw adres zwrotny na :ipn. Możesz skonfigurować oferty w panelu Dedipass.',
-
-    'keys' => 'Zdobądź klucze',
-
-    'site_money' => 'Aby Dedipass działał, musisz włączyć zakupy za pieniądze witryny w <a href=":url">ustawieniach sklepu</a>.',
-];
